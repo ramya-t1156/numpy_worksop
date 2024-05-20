52e35d016fad92f05c1fcc142901b458fefb12bf
ramya-t1156 <153516045+ramya-t1156@users.noreply.github.com>
2024-05-20 14:19:08 +0530

    question2.py

--- question2.py
+++ question2.py
@@ -1,1 +1,14 @@
 # find if the given number is a palindrome or not?
+def isPalindrome(n):
+  ans = n
+  rev = 0
+  while(n!=0):
+    rev = rev*10+n%10
+    n//=10
+  return n==rev
+n = int(input())
+if(isPalindrome(n)):
+  print(f"{n} is Palindrome")
+else:
+  print(f"{n} is not a Palindrome")
+  

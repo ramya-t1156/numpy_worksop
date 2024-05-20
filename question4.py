4ae7ac69cd7ea741f184fad1f5ca32cdae701568
ramya-t1156 <153516045+ramya-t1156@users.noreply.github.com>
2024-05-20 14:31:40 +0530

    question4.py

--- question4.py
+++ question4.py
@@ -1,1 +1,10 @@
 #write a program to find the sum of digits of a given number'
+def sumDig(n):
+  sum = 0
+  while(n!=0):
+    sum+=num%10
+    n//=10
+  return sum
+n = int(input())
+result = sumDig(n)
+print(result)
